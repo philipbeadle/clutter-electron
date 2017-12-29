db0f32775bee314709011acf73682eb2e835ba5f
Philip Beadle <philip.beadle@live.com.au>
2017-12-30 10:08:04 +1100

    Testing Electron app with Cypress and recording it on Cypress dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Follow from 'holochain-web-components';
+// import Follow from 'holochain-web-components';
 
 class App extends Component {
   render() {
